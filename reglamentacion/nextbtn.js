23cbb33d21dffc727e4b9e91904bdd3e4dfb7481
roarweb <rodriguezarrocha@gmail.com>
2025-08-09 14:03:13 -0500

    cambio annio

---
+++ reglamentacion/nextbtn.js
@@ -0,0 +1,12 @@
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+prevBtn.addEventListener('click', () => {
+  alert('Botón de retroceder presionado.');
+  // Aquí puedes agregar la lógica para retroceder
+});
+
+nextBtn.addEventListener('click', () => {
+  alert('Botón de avanzar presionado.');
+  // Aquí puedes agregar la lógica para avanzar
+});
